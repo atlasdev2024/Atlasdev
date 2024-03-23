a450c21fa6b4889914387cad3da623967c5880fb
rjmoraleda <atlas.dev.2024@gmail.com>
2024-03-23 19:53:51 +0800

    Checking of nav code error
    omitted Elements form documentElementElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   const [theme, setTheme] =React.useState
-  (localStorage.getItem("theme")? 
-  localStorage.getItem("theme"): "light"
+  (localStorage.getItem("theme") ? 
+  localStorage.getItem("theme") : "light"
   );
 
-  const element = document.documentElementElement;
+  const element = document.documentElement;
   useEffect(() => {
     if (theme=="dark") {
       element.classList.add("dark");
@@ -20,8 +20,7 @@ const App = () => {
   return (
   <div>
       <Navbar theme={theme} setTheme=
-      {setTheme}
-      />
+      {setTheme}/>
     </div>
   );
 };
